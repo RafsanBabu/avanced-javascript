eb2ed49e1bbd44c22419641e3c990f56c824f75b
Rafsan Babu <rafsan.cse.ru13@gmail.com>
2020-08-17 01:00:57 +0600

    null vs undefined explained

---
+++ undefinedvsnull.js
@@ -0,0 +1,15 @@
+/*let pakhi;
+console.log(pakhi);*/
+
+/*function add(num1, num2){
+    console.log(num1+num2);
+}
+
+const result= add(13,8);
+console.log(result);*/
+
+/*const premik = {name:"HHHHj" , age:300}
+console.log(premik.school);*/
+
+let  fun = undefined;
+console.log(fun);
