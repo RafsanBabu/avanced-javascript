63e6f4d2d7fe1564476b8266aa0e90611dc7ddbe
Rafsan Babu <rafsan.cse.ru13@gmail.com>
2020-08-17 01:14:56 +0600

    == vs === cleared

---
+++ doubleequalvstrippeequals.js
@@ -0,0 +1,21 @@
+//(==) kun variable kun type seti check korbe na
+const frist = 2;
+const second="2";
+if(frist==second){
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
+
+//(===)kstrictly chek kore ,type onusare compare kore
+const frist1 = 2;
+const second1="2";
+if(frist1===second1){
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
